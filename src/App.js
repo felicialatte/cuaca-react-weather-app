3fe9c17af6b80a98286660c59544b76c8a75c5cc
Felicia Audrey <audreyfelicia.feli@gmail.com>
2025-04-29 12:06:53 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
+     <h1>Weather App</h1>
+    <footer>
+      This project is created by <a href='https://github.com/felicialatte' target='_blank'>
+      Felicia Audrey
+      </a> and is {" "}
+      <a href='https://github.com/felicialatte/cuaca-react-weather-app' target='_blank'>
+      open-sourced on GitHub
+      </a>
+    </footer>
     </div>
   );
 }
